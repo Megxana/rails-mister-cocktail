921c350166ba748991cd005f51436e3a806d2a2a
Meg Bailey <megxbailey@gmail.com>
2020-08-20 17:14:14 +0100

    Ive done some super nice stuff here too

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy Cocktails!"
+Cocktail.destroy_all
+puts "DB is clean!"
+puts "Creating Cocktails"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Firewater")
+Ingredient.create(name: "Rum" )
+Ingredient.create(name: "Red wine")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "seltzers")
+Ingredient.create(name: "cola")
+Ingredient.create(name: "cherry")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "Bitters"
